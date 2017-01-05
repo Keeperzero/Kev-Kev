66b89c9f94ce98a66b1bf9d202af0abcedcaf06f
Keeperzero <Kevinomar138@gmail.com>
2017-01-05 11:26:11 +0100

    Bonjour

---
+++ aisdj.py
@@ -0,0 +1,7 @@
+# 1. r
+r = int(input("Gib eine Zahl für r ein: "))
+r = int(input("Gib eine Zahl für r ein: "))
+# 2. pi
+pi = 3.1415
+
+print("{0} * {1} * {2}=".format (str(r), str(r), str(pi)), pi * r * r)
---
+++ dreizahlen.py
@@ -0,0 +1,33 @@
+
+
+# 1. Nehme den ersten Gegenstand und lege ihn nach links
+left = 3
+
+# 2. Nehme den zweiten Gegenstand und lege ihn in die Mitte
+middle = 7
+
+# 3. Nehme den dritten Gegenstand und lege ihn nach rechts
+right = 1
+
+# 4. Wenn der linke größer als der mittlere ist, tausche die beiden
+if left > middle: left, middle = middle, left
+
+# 5. Wenn der linke größer als der rechte ist, tausche die beiden
+if left > right: left, right = right, left
+
+# 6. Wenn der mittlere größer als der rechte ist, tausche die beiden
+if middle > right: middle, right = right, middle
+
+print (left)
+print (middle)
+print (right)
+# 7. fertig.
+
+
+
+a*b = 7*2
+
+
+print (7*2)
+
+
